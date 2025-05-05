7b8ffc417dca13b201412209dad3443aebd7122b
Cainan <cainan.bas@gmail.com>
2025-05-05 08:53:56 -0300

    Começo do roteiro 07

---
+++ src/roteiro07/parte01/Component.java
@@ -0,0 +1,22 @@
+package roteiro07.parte01;
+
+public class Component {
+    private String nome;
+
+    public Component(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
+}
